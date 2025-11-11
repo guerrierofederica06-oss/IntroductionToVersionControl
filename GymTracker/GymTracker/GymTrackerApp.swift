a9df56f23c7b99832269c0ce534482ebba06006b
guerrierofederica06-oss <guerrierofederica06@gmail.com>
2025-11-11 09:43:59 +0100

    👽

---
+++ GymTracker/GymTracker/GymTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymTrackerApp.swift
+//  GymTracker
+//
+//  Created by Federica Guerriero on 09/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GymTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
